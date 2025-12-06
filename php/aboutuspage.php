cb0c5e1a7d8eb547765429fde7958cb55a2ad92b
Siti <sitichania237@gmail.com>
2025-12-07 00:32:20 +0700

    Tampilan responsif pada halaman about us

--- php/aboutuspage.php
+++ php/aboutuspage.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SIPAk - About Us</title>
     <link rel="stylesheet" href="../css/aboutuspage.css" />
 </head>
